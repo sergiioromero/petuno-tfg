ef2fa9a4dc2556d84175c7bc3627d5dcf906c9bd
Guhooo <hujo2581@gmail.com>
2026-05-14 14:20:02 +0200

    Notificaciones

--- lib/features/home/presentation/widgets/home_app_bar.dart
+++ lib/features/home/presentation/widgets/home_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import '../../../../core/theme/app_theme.dart';
 import '../../../../core/providers/theme_provider.dart';
